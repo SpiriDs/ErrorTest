7f93086819a865e483ece59f2f26810c7c516436
SpiriDs <spirids@live.de>
2020-06-25 11:05:02 +0700

    Add project files.

---
+++ ErrorTest/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ErrorTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MyButton
+            // 
+            this.MyButton.Location = new System.Drawing.Point(335, 112);
+            this.MyButton.Name = "MyButton";
+            this.MyButton.Size = new System.Drawing.Size(75, 23);
+            this.MyButton.TabIndex = 0;
+            this.MyButton.Text = "button1";
+            this.MyButton.UseVisualStyleBackColor = true;
+            this.MyButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MyButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MyButton;
+    }
+}
+
